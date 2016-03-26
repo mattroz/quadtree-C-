32431c029f07f0fd91faa2dc8ff77190a70b99ff
mattroz <matveyrozanov@gmail.com>
2016-03-26 12:28:41 +0300

    Define Quadtree class fields

--- WindowsFormsApplication1/Class1.cs
+++ WindowsFormsApplication1/Class1.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Data;
+using System.Drawing;
 
 public class Quadtree
 {
+    //constructor
 	public Quadtree()
 	{
 	}
+
+    //destructor
+    public ~Quadtree()
+    { 
+    }
+
+    //class fields
+    private int maximum_objects = 2;
+    private int number_of_children = 0;
+    private Point left_top_bound;
+    private Point right_bot_bound;
+    Quadtree parent = new Quadtree();
+    Quadtree north_west = new Quadtree();
+    Quadtree north_east = new Quadtree();
+    Quadtree south_west = new Quadtree();
+    Quadtree south_east = new Quadtree();
 }
